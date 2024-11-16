9aa3b8c0a518a28a68c9229f1b4a3a6f2f25664c
thomas0124 <qingshuitouzhen@gmail.com>
2024-11-16 16:41:17 +0900

    feat:OGPの設定

--- art-wine-bar/src/app/layout.tsx
+++ art-wine-bar/src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Art・WINE BAR competition",
-  description: "Art・WINE BAR competition",
+  title: "アートワインバー　コンペ",
+  description: "アートワインバー名古屋デザインコンペティション2024",
 };
 
 export default function RootLayout({
@@ -15,7 +15,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ja">
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="アートワインバー　コンペ" />
+      <meta property="og:description" content="アートワインバー名古屋デザインコンペティション2024" />
+      <meta property="og:url" content="https://competition-cst.vercel.app/" />
+      <meta property="og:site_name" content="アートワインバー コンペ" />
+      <meta property="og:image" content="https://portfolio-thomas0124.vercel.app/opengraph-image.png" />
+      <meta property="og:image:type" content="image/png" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
       <body className={inter.className}>{children}</body>
     </html>
   );
